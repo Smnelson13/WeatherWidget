be9a220a357aa84678081496790ab082e05bf5f8
Shane Nelson <swiftyshane@gmail.com>
2022-01-25 19:07:53 -0500

    initial commit

---
+++ WeatherWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherWidget
+//
+//  Created by Shane Nelson on 1/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherWidget/WeatherWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWidgetApp.swift
+//  WeatherWidget
+//
+//  Created by Shane Nelson on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
